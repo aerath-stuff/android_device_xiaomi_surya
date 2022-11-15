45a597ed6e2fd1dc94c6c7e6a3fe3e2f7ae717df
frknkrc44 <krc440002@gmail.com>
2022-11-16 00:25:38 +0300

    Fix mifare reader (thank you Vlad)
    Change-Id: I94baa66426ade3130a1da8b115c90e677a56b800

--- device.mk
+++ device.mk
@@ -310,7 +310,7 @@ PRODUCT_PACKAGES += \
     android.hardware.nfc@1.2-service
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
+    $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_COPY_FILES += \
