3d7afd04a283a6469fff0e68e0a9a14f081e8904
frknkrc44 <krc440002@gmail.com>
2022-11-29 17:22:22 +0300

    Add libpiex_shim back
    Change-Id: I3fd7ab4a90e5cfd394c42a66c92b2baa715ef5cf

--- device.mk
+++ device.mk
@@ -119,6 +119,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+    libpiex_shim
+
 # Device-specific settings
 PRODUCT_PACKAGES += \
     XiaomiParts
